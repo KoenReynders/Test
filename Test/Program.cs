1bebb40ea66c7945ef78a07afb315348dc1df380
Koen Reynders <koen.reynders1@gmail.com>
2015-04-22 14:25:35 +0200

    Typefout in GITHUB lokaal verbeterd

--- Test/Program.cs
+++ Test/Program.cs
@@ -12,7 +12,7 @@ namespace Test
         {
             Console.WriteLine("Regel ingetypt na eerste commit");
             Console.WriteLine("Derde commit");
-            Console.Writeline("Deze regel is op de GITHUB bijgevoegd");
+            Console.WriteLine("Deze regel is op de GITHUB bijgevoegd");
         }
     }
 }
