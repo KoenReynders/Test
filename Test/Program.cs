e9028e8e9f8d0f055514a0e392a5de7ca27383c4
Koen Reynders <koen.reynders1@gmail.com>
2015-04-22 14:27:40 +0200

    Efficiënter programma gemaakt

--- Test/Program.cs
+++ Test/Program.cs
@@ -13,6 +13,7 @@ namespace Test
             Console.WriteLine("Regel ingetypt na eerste commit");
             Console.WriteLine("Derde commit");
             Console.WriteLine("Deze regel is op de GITHUB bijgevoegd");
+            Console.WriteLine("Efficiënter programa in branch EfficiënterProgramma gemaakt");
         }
     }
 }
